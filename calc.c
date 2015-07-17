c40cb1ec843796c6ccf5f0531104432f0b196796
Hank Park <hank@smatoos.com>
2015-07-17 11:11:21 +0900

    fixed #3 add function prototypes.

--- calc.c
+++ calc.c
@@ -1,9 +1,30 @@
-int main()
+int main(int argc, char* argv[])
+{
+	int a, b, c;
+	char operator;
+
+	printf("Please enter an expression:");
+	scanf("%d %c %d", &a, &operator, &b);
+
+	
+}
+
+int add(int a, int b)
 {
 
 }
 
-int add(int a, int b)
+int del(int a, int b)
+{
+
+}
+
+int mul(int a, int b)
+{
+
+}
+
+double div(int a, int b)
 {
 
 }
